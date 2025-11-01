6510b1f8a0bda9487d0f09ef493e3d36c1d71763
BoffinsBlog <236779735+BoffinsBlog@users.noreply.github.com>
2025-11-01 11:42:34 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GitHub
+
+## Scripts for GitHub
+
+Read more at [Boffin's Blog](https://boffinsblog.github.io/)
---
+++ init-update/git_update_everything.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Variables
+repo_Root=/home/foo/path/to/repos
+github_URL=<SSH_KEY_NAME>:<GITHUB_ACCOUNT>
+
+clear
+
+# Push everything to GitHub
+echo '';
+echo -e '\e[1;34mPush everything to GitHub...\e[0m';
+echo '';
+
+function updateGit {
+    local -n arr=$1
+
+    for i in "${arr[@]}"
+    do
+
+    # Update the remote git repo
+    echo -e '\e[1;33mUpdate the remote' $i 'git repo...\e[0m'
+
+    cd $repo_Root/$i/
+    git add .
+    git commit -m 'Modified' .;
+    git push -u origin main;
+
+    # Done
+    echo -e '\e[1;32m'$i 'done\e[0m'
+    echo ''
+
+    done
+}
+
+# Define the array of all the directory names
+declare -a repo_Dirs
+
+for repo_Dir in $repo_Root/*/; do
+    dir_path=$repo_Dir
+    dir_name=$(basename $dir_path)
+    repo_Dirs+=("$dir_name")
+done
+
+# Loop
+updateGit repo_Dirs
+
+# Everything done
+echo -e '\e[1;34mEverything done\e[0m';
+
